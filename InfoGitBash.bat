8e56bc01ddbcc65594f674abbb7b97b7a3b689cc
Roberto Carlos <rcp102.rcp@gmail.com>
2021-10-31 17:56:06 -0600

    Add infoGitBash.bat

---
+++ InfoGitBash.bat
@@ -0,0 +1,7 @@
+cd  Document    # Lleva al directorio
+cd ..           # Regresa una carpeta atras
+ls              # Conocer el contenido del directorio
+mkdir test      # Crea el directorio test
+rmdir test      # Elimina el directorio test
+touch test.txt  # Crea el archivo test.txt
+rm test.txt     # Elimina el archivo test.txt
